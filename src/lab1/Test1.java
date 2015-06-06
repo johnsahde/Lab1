3ddbbf45d925cdc55a8447b858a711d4ddd38a6b
ThothsCoder <karim_rodrigo@outlook.com>
2015-06-06 13:41:54 -0500

    Update Test1.java
    Averigua

--- src/lab1/Test1.java
+++ src/lab1/Test1.java
@@ -2,7 +2,7 @@ package lab1;
 
 public class Test1 {
   public static void main(String[] args) {
-    System.out.println("What's wrong with this program?");
+    System.out.println("What's wrong with this program? yo cambie esto");
   }
 }
 class TestAnother1 {
