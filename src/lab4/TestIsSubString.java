ccf875e07a6e80769d9422c79e9dd00de43bcd97
johnsahde <john_sahde@hotmail.com>
2015-06-08 00:04:27 -0500

    Update TestIsSubString.java
    Limpiar código

--- src/lab4/TestIsSubString.java
+++ src/lab4/TestIsSubString.java
@@ -29,110 +29,13 @@ public class TestIsSubString {
     } // end main
 
     private static boolean isSubString(String s, String x) {
-//        // Version 1
-//        boolean found = false;
-//        int c = 0, j=0;
-//        out:
-//        for (int i = 0; i <= (s.length() - 1); ) {
-//                do{
-//                    if (j <= (x.length() - 1)) {
-//                        
-//                        if (s.charAt(i) == x.charAt(j)) {
-//                            found = true;
-//                            c++;    
-//                        }
-//
-//                        else if(c > 0 && found && c ==(s.length()-1)){
-//                            found = false;    
-//                            break out;
-//                        }
-//                        else {
-//                            found = false;
-//                            i=0;
-//                            c=0;
-//                        }
-//
-//
-//                        j++;
-//                        
-//                    }
-//                    else {
-//                        found = false;
-//                        break out;
-//                    }
-//                    
-//                    
-//                } while(j < (x.length()-1) && !found);
-//            if (j == (x.length()-1) && !found) {
-//                 found = false;
-//                 break;
-//            }
-//            if (found && c == (s.length()-1) && j > (x.length()-1)) {
-//                found = false;
-//                break;
-//            }
-//            i++;
-//        }
-//        
-//        return found;
-//    }
-        
-//         // Version 2 ESTABLE
-//        boolean found = false;
-//        int c = 0, j =0;
-//        int subStringLength = s.length() - 1;
-//        int stringLength = x.length() - 1;
-//        out:
-//        for (int i = 0; i <= subStringLength; ) {
-//                do{
-//                    if (j <= stringLength) {
-//                        
-//                        if (s.charAt(i) == x.charAt(j)) {
-//                            found = true;
-//                            c++;    
-//                        }
-//                      else if(c > 0 && found && c == subStringLength + 1){
-//                      //  else if(found && c == subStringLength + 1){
-//                            found = false;    
-//                            break out;
-//                        }
-//                        else {
-//                            found = false;
-////                            i=0;
-////                            c=0;
-//                            i=c=0;
-//                        }
-//
-//
-//                        j++;
-//                        
-//                    }
-//                    else {
-//                        found = false;
-//                        break out;
-//                    }
-//                    
-//                    
-//                } while(j < stringLength && !found);
-//            if (j == stringLength && !found) {
-//                 found = false;
-//                 break;
-//            }
-//            if (found && c == subStringLength && j > stringLength) {
-//                found = false;
-//                break;
-//            }
-//            i++;
-//        }
-//        
-//        return found;
-//    }
-        
+
          // Version 3 ESTABLE BUENA
         boolean found = false;
         int c = 0, j =0;
         int subStringLength = s.length() - 1;
         int stringLength = x.length() - 1;
+        
         outer:
         for (int i = 0; i <= subStringLength; i++) {    //Recorrido de subcadena
                 do {                                    //Recorrido de cadena
@@ -164,44 +67,3 @@ public class TestIsSubString {
         return found;
     }// end isSubString function
 }// end TestIsSubString class
-    
-//     // Version 4 NOT FUNCTIONAL
-//        boolean found = false;
-//        int c = 0, j =0;
-//        int subStringLength = s.length() - 1;
-//        int stringLength = x.length() - 1;
-//        out:
-//        for (int i = 0; i <= subStringLength; i++) {
-//                
-//                    while (j <= stringLength && !found) {
-//                        
-//                        if (s.charAt(i) == x.charAt(j)) {
-//                            found = true;
-//                            c++;    
-//                        }
-//                        else if ( found && c == subStringLength + 1) {
-//                                found = false;    
-//                                break out;
-//                        }
-//                        else {
-//                            found = false;
-//                            i=c=0;
-//                        }
-//                        
-//                        j++;
-//                    }
-//                    
-//                    if (j > stringLength) {
-//                        found = false;
-//                        break;
-//                    }
-//                    
-//                    
-//                    
-//               
-//        }
-//        
-//        return found;
-//    }
-        
-       
